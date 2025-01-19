149f425191a245d6ba399ba971c33086fef5ccca
eeee0717 <chentao020717Work@outlook.com>
2025-01-19 16:52:59 +0800

    feat: finish show processed image

--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -1,38 +1,45 @@
 <script setup lang="ts">
-const fileName = ref<string | null>(null)
-const filePath = ref<string | null>(null)
-const originalImage = ref<string | null>(null)
+import { ref } from 'vue'
 
+// State management
+const fileName = ref<string | null>(null)
+const originalImage = ref<string | null>(null)
+const processedImageUrl = ref<string | null>(null)
+
+// File handling
 function onFileChange(e: Event) {
-  const target = e.target as HTMLInputElement
-  const file = target.files?.[0]
-  if (file) {
-    fileName.value = file.name
-    filePath.value = `${import.meta.env.VITE_ROOT_PHOTO_PATH}/${fileName.value}`
-    const reader = new FileReader()
-    reader.onload = (e) => {
-      if (e.target) {
-        originalImage.value = e.target.result as string
-      }
-    }
-    reader.readAsDataURL(file)
+  const file = (e.target as HTMLInputElement).files?.[0]
+  if (!file)
+    return
+
+  fileName.value = file.name
+  const reader = new FileReader()
+  reader.onload = (e) => {
+    originalImage.value = e.target?.result as string
   }
+  reader.readAsDataURL(file)
 }
 
+// API interaction
 async function sendImage() {
   if (!fileName.value)
     return
 
-  fetch('http://localhost:3000/test', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ file_path: filePath.value }),
-  })
-    .then((res) => {
-      console.warn(res)
+  try {
+    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/test`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ file_name: fileName.value }),
     })
+
+    const { processed_file_name } = await response.json()
+    processedImageUrl.value = `${import.meta.env.VITE_BASE_URL}/assets/${processed_file_name}`
+  }
+  catch (error) {
+    console.error('Error processing image:', error)
+  }
 }
 </script>
 
@@ -43,9 +50,17 @@ async function sendImage() {
       <button :disabled="!fileName" @click="sendImage">
         上传并处理
       </button>
-      <div v-if="fileName" class="mt-4 flex flex-col items-center">
+    </div>
+
+    <div class="mt-4 flex flex-row justify-center gap-4">
+      <div v-if="fileName" class="image-box mt-4 flex flex-col items-center">
         <p>选择的文件: {{ fileName }}</p>
         <img v-if="originalImage" class="h-auto w-200px" :src="originalImage" alt="原图">
+      </div>
+
+      <div v-if="processedImageUrl" class="image-box mt-4 flex flex-col items-center">
+        <p>处理后的文件</p>
+        <img class="h-auto w-200px" :src="processedImageUrl" alt="处理后的图片">
       </div>
     </div>
   </div>
